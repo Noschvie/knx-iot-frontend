304d565c429bc131708b334df375dd798f87e77b
noschvie <48217146+Noschvie@users.noreply.github.com>
2026-09-23 13:50:43 +0200

    feat: Add token endpoint configuration for production and development environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
     production: true,
     apiBase: 'http://bff:3000',
     wsBase: 'ws://bff:3000',
+    tokenEndpoint: '/oauth/access',
     logging: {
         enableConsole: false,
         logLevel: 'error'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
     production: false,
     apiBase: 'http://localhost:3000',
     wsBase: 'ws://localhost:3000',
+    tokenEndpoint: '/oauth/access',
     logging: {
         enableConsole: true,
         logLevel: 'debug'
